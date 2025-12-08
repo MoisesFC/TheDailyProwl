fa97cbc49344a79e3bf581bb310405a1a5409d80
MoisesFC <figueroa.abe@outlook.com>
2025-12-08 00:45:42 -0500

    adding page 7

--- The Daily Prowl/src/ThirdPage.jsx
+++ The Daily Prowl/src/ThirdPage.jsx
@@ -9,7 +9,7 @@ import upcommingEventsButton from './assets/Page3/DP6_0003_Upcoming-Events.png';
 import yourScheduleButton from './assets/Page3/DP6_0004_Your-Schedule.png';
 import ksuClubsButton from './assets/Page3/DP6_0005_KSU-Clubs.png';
 import latestNewsButton from './assets/Page3/DP6_0007_Latest-News.png';
-import bucketListButton from './assets/Page3/bucketList.png';
+import bucketListButton from './assets/Page3/bucketlist.png';
 
 function ThirdPage() {
   const navigate = useNavigate();
@@ -105,6 +105,7 @@ function ThirdPage() {
         <img
           src={latestNewsButton}
           alt="Latest News"
+          onClick={() => window.open('https://owllife.kennesaw.edu/', '_blank')}
           style={{
             width: '250px',
             height: 'auto',
